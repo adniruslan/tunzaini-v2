91bf16b5b73d9aa647e77be0c91e43e7e6659577
adniruslan <ruslanadni@gmail.com>
2024-08-17 23:53:57 +0800

    First commit

---
+++ plugins/whatsapp/js/whatschat-style10.js
@@ -0,0 +1,31 @@
+/*======================================================
+
+Project: WhatsChat - WhatsApp Chat Widget jQuery Plugin
+Author: Black Theme
+Released On: 4, Sep 2019
+@version: 1.0
+
+========================================================*/
+
+$(document).ready(function(){
+
+    //click event on a tag
+    $('.wc-list').on("click",function(){
+     
+      var number =  $(this).attr("data-number");
+      var message =  $(this).attr("data-message");
+      
+      //checking for device type
+      if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+          // redirect link for mobile WhatsApp chat awc
+          window.open('https://wa.me/'+number+'/?text='+message, '-blank');  
+      }
+      else{
+          // redirect link for WhatsApp chat in website
+          window.open('https://api.whatsapp.com/send/?phone='+number+'&text='+message, '-blank&app_absent=0'); 
+          
+      }
+    })
+});
+
+
